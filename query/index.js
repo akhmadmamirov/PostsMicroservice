685a3a766aaca5fdbf3a1eafac0a04ccc827c7d1
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2023-07-08 11:51:54 -0400

    adding query service

---
+++ query/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+const posts = {}
+
+app.post('/events', (req, res) => {
+    const {type, data} = req.body;
+
+    if (type === 'PostCreated') {
+        const {id, title} = data;
+        posts[id] = {id, title, comments: []};
+        
+    }
+    if (type === 'CommentCreated') {
+        const {postId, content, id} = data;
+        const post = posts[postId];
+        post.comments.push({id, content})
+    }
+
+    res.status(200).send({});
+})
+
+app.get('/events', (req, res) => {
+    res.status(200).send(posts)
+})
+
+app.listen(4002, () => {
+    console.log('Listening on port: 4002');
+})
